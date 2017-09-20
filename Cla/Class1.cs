60a83cfceb0c70ff61489fc00aef5bffe077cacd
xingforever <1003863495@qq.com>
2017-09-20 15:56:18 +0800

    9.20

---
+++ Cla/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cla
+{
+    public static  class Class1
+    {
+        public static int Add(int i,int j)
+        {
+           
+            return i + j;
+        }
+
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cla;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2, Class1.Add(1, 1));
+            Assert.AreEqual(1, Class1.Add(1, 0));
+            Assert.AreEqual(4, Class1.Add(0, 4));
+        }
+    }
+}
