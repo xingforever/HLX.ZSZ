fc548f2d9572a5cc3d38742b37cb27540c4c8482
xingforever <1003863495@qq.com>
2017-10-02 09:20:18 +0800

    10.2

---
+++ HLX.ZSZ.AdminWeb/Controllers/PermissionController.cs
@@ -0,0 +1,27 @@
+﻿using HLX.ZSZ.CommonMVC;
+using HLX.ZSZ.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HLX.ZSZ.AdminWeb.Controllers
+{
+    public class PermissionController : Controller
+    {
+        public IPermissionService  PermissionService { get; set; }
+        // GET: Permission
+        public ActionResult List()
+        {
+            var perms= PermissionService.GetAll();
+
+            return View(perms);
+        }
+        public  ActionResult Delete2(long  id)
+        {
+            PermissionService.MarkDeleted(id);
+            return Json(new AjaxResult { Status = "ok", Data = "", ErrorMsg = "" });
+        }
+    }
+}
